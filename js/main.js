c3d812ec11c8dd7cec41843fe490d96f22b98cb9
Mickey <mickey@Mickeyde-MacBook-Air.local>
2018-06-02 16:09:01 +0800

    complete animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+var wrapper = document.getElementById('wrapper');
 var video = document.getElementById('video');
+var header = document.getElementById('header');
+var contant = document.getElementById('contant');
 
-video.addEventListener('click', addlandingcss);
+video.addEventListener('click', changewrapper);
+video.addEventListener('click', addvideocss);
+video.addEventListener('click', addheadercss);
+video.addEventListener('click', addcontantcss);
 
-function addlandingcss() {
-     video.className = 'landingbox-next';
+function changewrapper() {
+  wrapper.className = 'wrapper-next';
+}
+
+function addvideocss() {
+     video.className = 'video-wrapper-next';
+
+}
+
+function addheadercss() {
+  header.className = 'header-next';
+}
+
+function addcontantcss() {
+  contant.className = 'contant-next';
 }
