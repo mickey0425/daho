cab89e23e0e09aaaa6abd853599f419eedaab604
Mickey <mickey@Mickeyde-MacBook-Air.local>
2018-06-15 15:40:01 +0800

    new index

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
+//wrapper-video
 var wrapper = document.getElementById('wrapper');
 var video = document.getElementById('video');
+
+//header & hambugerlist
 var header = document.getElementById('header');
+var menuButton = document.getElementById('menuButton');
+var menulist = document.getElementById('menulist');
+var menu = document.getElementById('menu');
 var contant = document.getElementById('contant');
+
+//cloth
+var clothleft = document.getElementById('clothleft');
+var clothright = document.getElementById('clothright');
+
+
 
 video.addEventListener('click', changewrapper);
 video.addEventListener('click', addvideocss);
 video.addEventListener('click', addheadercss);
 video.addEventListener('click', addcontantcss);
 
+menuButton.addEventListener('click', openMenu);
+menuButton.addEventListener('click', changeIcon);
+
+video.addEventListener('click', ClothOn);
+
 function changewrapper() {
   wrapper.className = 'wrapper-next';
 }
 
 function addvideocss() {
-     video.className = 'video-wrapper-next';
+  video.className = 'video-wrapper-next';
 
 }
 
@@ -23,4 +40,38 @@ function addheadercss() {
 
 function addcontantcss() {
   contant.className = 'contant-next';
+}
+
+function openMenu() {
+  if (menu.className.indexOf('menu open') >= 0) {
+    menu.className = 'menu';
+  } else {
+    menu.className = 'menu open';
+  }
+}
+
+function changeIcon() {
+
+  if (menuButton.className.indexOf('menu-button close') >= 0) {
+    menuButton.className = 'menu-button';
+    menulist.className = 'menu-list';
+
+  } else {
+    menuButton.className = 'menu-button close';
+    menulist.className = 'menu-list-next';
+  }
+
+}
+
+//go to about
+function gotoabout() {
+  window.scrollTo({
+    top: 500,
+    behavior: "smooth"
+});
+}
+
+function ClothOn() {
+  clothright.className = 'cloth-right-on';
+  clothleft.className = 'cloth-left-on';
 }
